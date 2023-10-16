b3ff6583eb5fc4cfe949c1aea3e728b8c009e5de
Harshika Anand <142821111+Harshika-Anand@users.noreply.github.com>
2023-10-16 16:06:44 +0530

    Printing methods

---
+++ p2.dart
@@ -0,0 +1,25 @@
+1)
+  void main() {
+ print("2+2");
+}  
+
+Output: 2+2
+
+  
+
+2)
+  void main() {
+ print("2"+"2");
+}  
+
+Output: 22
+
+
+
+  3)
+  void main() {
+ print(2+2);
+}  
+
+Output: 4
+  
